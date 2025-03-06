6fafb817dfbc366aea122be3d5c412fc0d798ccb
Harshamasina <harshamasina1999@gmail.com>
2025-03-06 09:26:48 -0600

    added: "Error 404"

--- src/Pages/Error404.jsx
+++ src/Pages/Error404.jsx
@@ -1,8 +1,17 @@
+import error_404 from "../assets/error_404.jpg";
+import { Link } from 'react-router-dom';
+
 const Error404 = () => {
     return (
-        <div>
-            <h1>Error 404</h1>
-        </div>
+        <section className="error404_container">
+            <div className="error404_img">
+                <img src={error_404} alt="error 404" />
+            </div>
+
+            <div className="error404_link_container">
+                <Link to="/" className="error404_link">Go To Home Page</Link>
+            </div>
+        </section>
     )
 };
 
